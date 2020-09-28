6758e0d0dd31bac9647a3b9879aeafb799e534b0
Tit Petric <tit.petric@monotek.net>
2020-09-28 11:34:23 +0200

    strip comments from sql statements as detected, to allow them anywhere, even on one line statements

--- migrate/statements.go
+++ migrate/statements.go
@@ -11,12 +11,18 @@ func statements(contents []byte, err error) ([]string, error) {
 		return result, err
 	}
 
-	stmts := regexp.MustCompilePOSIX(";$").Split(string(contents), -1)
+	// remove sql comments from anywhere ([whitespace]--*\n)
+	comments := regexp.MustCompile(`\s*--.*`)
+	contents = comments.ReplaceAll(contents, nil)
+
+	// split statements by trailing ; at the end of the line
+	stmts := regexp.MustCompilePOSIX(`;$`).Split(string(contents), -1)
 	for _, stmt := range stmts {
 		stmt = strings.TrimSpace(stmt)
 		if stmt != "" {
 			result = append(result, stmt)
 		}
 	}
+
 	return result, nil
 }
